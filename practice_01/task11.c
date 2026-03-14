b4f0fbb742859c68e41b3641ad3404cc55d4c3be
dyxvill <dyxvill@mail.ru>
2026-03-14 14:35:21 +0300

    сделала 11

---
+++ practice_01/task11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a;
+    scanf("%d", &a);
+    if (a % 2 == 0)
+    {
+        printf("Ваше число чётное");
+    }
+    else
+    {
+        printf("Ваше число нечётное");
+    }
+    return 0;
+}
