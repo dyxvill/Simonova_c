c9e9808760cf144c807c2eeca66a2175a7a1da4d
dyxvill <dyxvill@mail.ru>
2026-03-14 15:17:02 +0300

    сделала 12

---
+++ practice_01/task12.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+    int a
+    int b
+    int c
+    scanf("%d", &a)
+    scanf("%d", &b)
+    scanf("%d", &c)
+    if ( a > b && a > c)
+    {
+        printf("Максимальное = %d", a);
+    }
+    else if ( b > c && b > a)
+    {
+        printf("Максимальное = %d", b);
+    }
+    else
+} 
