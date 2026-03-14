4271f362daea022054d8c57c355a67ff478977ae
dyxvill <dyxvill@mail.ru>
2026-03-14 11:25:12 +0300

    сделала 3 задание

---
+++ practice_01/task3.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() {
+    printf("sizeof(char) = %lu \n", sizeof(char));
+	printf("sizeof(float) = %lu \n", sizeof(float));
+	printf("sizeof(double) = %lu \n", sizeof(double));
+}
