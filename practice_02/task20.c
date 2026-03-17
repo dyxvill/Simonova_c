78e1e9f1988cb1f6d13c917e3f852202c6895c7d
dyxvill <dyxvill@mail.ru>
2026-03-17 14:17:26 +0300

    20-e

---
+++ practice_02/task20.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int i = 1;
+    for (; i <= 100; i++)
+    {
+        if ( i % 3 == 0 && i % 5 == 0 )
+        {
+            printf("%d = FizzBuzz \n", i);
+        }
+        else if ( i % 3 == 0)
+        {
+            printf("%d = Fizz \n", i);
+        }
+        else if ( i % 5 == 0)
+        {
+            printf("%d = Buzz \n", i);
+        }
+        else
+        {
+            printf("%d \n", i);
+        }
+    }
+    return 0;
+}
