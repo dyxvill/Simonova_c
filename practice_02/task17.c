33aeb22dc6fdfe2915c26d4f754aa50aa005bc63
dyxvill <dyxvill@mail.ru>
2026-03-17 13:59:02 +0300

    17-e

---
+++ practice_02/task17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    int count = 0;
+    while (N != 0)
+    {
+        if ( N > 0 ) 
+        {
+            count = count + 1;
+        }
+        scanf("%d", &N);
+    }
+    printf("количество = %d", count);
+    return 0;
+}
