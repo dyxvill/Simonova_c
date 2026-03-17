c78335eaad1f68b97386539e98e89f9ec323a516
dyxvill <dyxvill@mail.ru>
2026-03-17 14:39:53 +0300

    22-e

---
+++ practice_02/task22.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    int i = 1;
+    for (; i <= N; i++)
+    {
+        if ( N % i == 0)
+        {
+            printf("%d \n", i);
+        }
+    }
+    return 0;
+}
