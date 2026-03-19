500e40dc05219e92984e3ec9cced9ed909cf771b
dyxvill <dyxvill@mail.ru>
2026-03-19 21:24:14 +0300

    34-e

--- practice_02/task34.c
+++ practice_02/task34.c
@@ -4,6 +4,10 @@ int main() {
     scanf("%d", &N);
     for (int i = 0; i < N; i++)
     {
+        for (int c = 0; c < N - i; c++)
+        {
+            printf(" ");
+        }
         for (int b = 1; b <= i; b++)
         {
             printf("%d", b);
