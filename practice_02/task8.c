b435e8ff33fd197e1a1c592ae05362822fc4083b
dyxvill <dyxvill@mail.ru>
2026-03-19 13:39:29 +0300

    8-e

---
+++ practice_02/task8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int i = 1;
+    int N;
+    scanf("%d", &N);
+    int sum = 0;
+    for (; i <= N; i++ )
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + i;
+        }
+    }
+    printf("%d", sum);
+    return 0;
+}
