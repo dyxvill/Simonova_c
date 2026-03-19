6493d4a465956c85b4edf89c2021ea0ce1763958
dyxvill <dyxvill@mail.ru>
2026-03-19 18:25:12 +0300

    35-e

---
+++ practice_02/task35.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    for (int i = 0; i <= 10; i++ )
+    {
+        for (int c = 1; c <= 10; c++)
+        {
+            if (c * i > 30) continue;
+            {
+            printf("% d * %d = %d \n", c, i, c * i);
+            }
+        }
+    }
+    return 0;
+}
